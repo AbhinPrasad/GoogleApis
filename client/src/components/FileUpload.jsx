f031c2824b781d9ebeb3f62c24d20ed25467276f
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-04-26 18:06:52 +0530

    file input-ui

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { FaGoogleDrive } from "react-icons/fa";
 
 const FileUpload = () => {
-  return (
-    <div>FileUpload</div>
-  )
-}
+	return (
+		<div className="fileUpload">
+			<form className="form">
+				<span className="form-title">Upload your file</span>
+				<p className="form-paragraph">Maximum size 50 mb</p>
+				<label htmlFor="file-input" className="drop-container">
+					<span className="drop-title">Drop files here</span>
+					or
+					<input
+						type="file"
+						id="file-input"
+                        name="file"
+					/>
+				</label>
+				<button className="btn">
+					<FaGoogleDrive /> Upload to Drive
+				</button>
+			</form>
+		</div>
+	);
+};
 
-export default FileUpload
+export default FileUpload;
